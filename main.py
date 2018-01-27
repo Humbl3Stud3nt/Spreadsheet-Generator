b3a18dd9f3aa8bdb644581a15c3bfe1081238265
Humbl3Stud3nt <waratjam@gmail.com>
2018-01-26 20:19:23 -0500

    Spreadsheet generation basics complete

--- main.py
+++ main.py
@@ -5,39 +5,36 @@ Created on Sat Jan 20 19:34:02 2018
 @author: warat
 """
 import spreadsheetGenerator
+import club
 
 NAME_QUERY = "Enter name of member: "
 FORM_QUERY = "Enter member form in format <grade level>-<class number>:\n"
 DUES_QUERY = "Enter dues given this meeting: $"
 
+QUERIES = (NAME_QUERY, FORM_QUERY, DUES_QUERY)
+
+CLUBS = ("Science", "Floetics")
 TITLES = ("FIRST NAME", "LAST NAME", "FORM", "DUES")
 
-global memberList
-memberList = []
 
+def getInfo():
+    memberList = []
 
-class Member:
-    def __init__(self, **kwargs):
-        self.club = kwargs["club"]
-        name = kwargs["name"].split(" ")
-        self.firstName = name[0].capitalize()
-        self.lastName = name[1].capitalize()
-        self.duesPaid = kwargs["duesPaid"]
-        self.form = kwargs["form"]
-
-
-def genMember(infoDict):
-
-    global memberList
-    memberList.append(Member(infoDict))
+    pass
 
 
 def main():
     pass
 
     
-if __name__ == "__main__":
-    main()
-                
-            
-    
+# if __name__ == "__main__":
+#     main()
+
+
+# a = Member()
+# print("First Name: " + a.firstName,
+#       "\nLast Name: " + a.lastName,
+#       "\nDues Paid: $" + str(a.duesPaid),
+#       "\nForm: " + a.form,
+#       "\nClub : " + a.club
+#       )
--- spreadsheetGenerator.py
+++ spreadsheetGenerator.py
@@ -9,48 +9,82 @@ Problem Statement: Design a command line program that accepts input and stores i
 """
 
 import openpyxl
+from openpyxl.styles import NamedStyle, Font
 import datetime
-#import os
+import club
+# import os
 
 
-def genSpreadsheet(spreadsheetTitle, titles):
-    """
-    Creates a spreadsheet and populates it with titles
-    
-    spreadsheetTitle -> title of spreadsheet
-    titles -> list of titles for spreadsheet; for now must be 26 or fewer titles
-    """
-    
-    # ensures that titles is a list or tuple
-    
+def genSpreadsheet(memberList, directory=""):
+    # ERROR HANDLING
     try:
-        assert type(titles) == list or type(titles) == tuple
-        
-    except:
-        print("Please use a list or tuple")
-        return None
-     
-    # ensures that there are 26 or fewer elements in titles        
-    try:
-        assert len(titles) <= 26
-        
-    except:
-        print("Limit number of titles to 26")
-        return None
-    
-    
+        assert type(memberList) == list
+
+    except AssertionError:
+        print("memberList should be of type 'list'.")
+        return 0
+
+    for member in memberList:
+        try:
+            assert type(member) == club.Member
+
+        except AssertionError:
+            print("INCONSISTENT RECORDS\n"
+                  "ABORTING SPREADSHEET CREATION")
+            return 0
+
+    clubName = memberList[0].club
+
+    for member in memberList:
+        try:
+            assert member.club == clubName
+        except AssertionError:
+            print("INCONSISTENT CLUB\n"
+                  "ABORTING SPREADSHEET CREATION")
+            return 0
+
     wb = openpyxl.Workbook()
-    
+
     sheet = wb.get_sheet_by_name("Sheet")
-    
-    
-    columns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    
-    for index in range(0,len(titles)):
-        sheet[columns[index] + "1"] = titles[index]
-        
+
+    def _indexToColumn(index):
+        headerBases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        try:
+            assert 0 <= index <= 26 and type(index) == int
+        except AssertionError:
+            print("Please limit number of attributes to 26")
+            return 0
+
+        return headerBases[index]
+
+    attributes = club.Member().attributes
+
+    titleFont = Font(bold=True, size=16)
+    titleStyle = NamedStyle(font=titleFont)
+
+    for i in range(len(attributes)):
+        column = _indexToColumn(i)
+        # print(column)
+        sheet[column + "1"] = attributes[i]
+
+        sheet.column_dimensions[column].width = 20
+
+    for i in range(len(memberList)):
+        row = str(i+2)
+        sheet["A" + row] = memberList[i].firstName
+        sheet["B" + row] = memberList[i].lastName
+        sheet["C" + row] = memberList[i].form
+        sheet["D" + row] = memberList[i].duesPaid
+
     currentDate = str(datetime.date.today())
-    fileName = currentDate + ".xlsx"
-    
+    fileName = directory + clubName + "-" + currentDate + ".xlsx"
+
     wb.save(fileName)
 
+
+clubList = []
+
+for i in range(10):
+    clubList.append(club.Member())
+
+genSpreadsheet(clubList)
